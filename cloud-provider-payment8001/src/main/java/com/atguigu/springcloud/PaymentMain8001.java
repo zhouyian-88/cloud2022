6cfd888bcb1872d68b2a56abb74aa2d49576edf2
yianzhou <2873642764@qq.com>
2022-11-22 21:46:18 +0800

    支付模块8001端口

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @athor:zhouhaohui
+ * @email:2873642764@qq.com
+ * @desc:
+ * @datetime:2022-11-22-20:31
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,50 @@
+package com.atguigu.springcloud.controller;
+
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import com.atguigu.springcloud.service.PaymentService;
+
+import javax.annotation.Resource;
+
+/**
+ * @athor:zhouhaohui
+ * @email:2873642764@qq.com
+ * @desc:
+ * @datetime:2022-11-21-22:37
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Resource
+    private PaymentService paymentService;
+    @Value("${server.port}")
+    private String serverPort;
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment){
+        int result = paymentService.create(payment);
+
+        log.info("******插入结果{}", result);
+        if(result > 0){
+            return new CommonResult(200,"插入数据成功,serverPort", serverPort+result);
+        }else{
+            return new CommonResult(444,"插入数据库失败",null);
+        }
+
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id")Long id){
+        Payment payment = paymentService.getPaymentById( id);
+        log.info("********查询结果:{}", payment);
+        if(payment != null){
+            return new CommonResult(200,"查询成功,serverPort", serverPort+payment);
+        }else{
+            return new CommonResult(444,"没有对应记录，查询ID:"+id,null);
+        }
+
+    }
+}
