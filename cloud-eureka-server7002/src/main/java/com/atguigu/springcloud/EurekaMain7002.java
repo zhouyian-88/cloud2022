3e6fb3b2bced2e22f26325e1bc11004736ace3b2
yianzhou <2873642764@qq.com>
2022-11-22 21:45:38 +0800

    Eureka模块7002端口

---
+++ cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @athor:zhouhaohui
+ * @email:2873642764@qq.com
+ * @desc:
+ * @datetime:2022-11-22-16:57
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
